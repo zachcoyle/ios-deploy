684665d50a30e58f56a046b837a05f140e87c313
Deva Ramasubramanian <rdeva@apple.com>
2018-04-16 15:19:38 -0700

    Use semver to properly validate versions
    Otherwise "10.0" is deemed to be smaller than "7.0", resulting in
    strange install errors.

--- src/scripts/check_reqs.js
+++ src/scripts/check_reqs.js
@@ -3,6 +3,7 @@
 var util = require('util');
 var os = require('os');
 var child_process = require('child_process');
+var semver = require('semver')
 
 var XCODEBUILD_MIN_VERSION = '7.0';
 var XCODEBUILD_NOT_FOUND_MESSAGE = util.format('Please install Xcode version %s or greater from the Mac App Store.', XCODEBUILD_MIN_VERSION);
@@ -28,7 +29,7 @@ xcode_version.on('close', function (code) {
 		var arr = version_string.match(/^Xcode (\d+\.\d+)/);
 		var ver = arr[1];
 
-		if (os.release() >= '15.0.0' && ver < '7.0') {
+		if (semver.gte(os.release(), '15.0.0') && semver.lt(ver + '.0', '7.0.0')) {
 			console.log(util.format('You need at least Xcode 7.0 when you are on OS X 10.11 El Capitan (you have version %s)', ver));
 			process.exit(1);
 		}
